ff7992ed07512f9f65dbd147e606e1326ba05110
monaim3 <monaimmukul75@gmail.com>
2022-12-06 12:16:30 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 import Header from './Component/Header/Header';
 import Product from './Component/Products/Product';
 import Products from './Component/Product/Products';
+import Navbar from './Component/Navbar/Navbar';
 
 function App() {
   return (
     <div>
+      <Navbar></Navbar>
       <Header></Header>
       <Products></Products>
+      
     </div>
   );
 }
